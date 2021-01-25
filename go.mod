98c800b4fbf872bde50730952ea4498fcc0035bb
Danny Hermes <daniel.j.hermes@gmail.com>
2021-01-24 23:42:21 -0600

    Adding implementation to `postgres` package and an example.

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/dhermes/tcp-replay-reverse-proxy
 go 1.15
 
 require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.7
 	github.com/spf13/cobra v1.1.1
 	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5
 	golang.org/x/tools v0.1.0
--- postgres/parse.go
+++ postgres/parse.go
@@ -11,3 +11,222 @@
 // limitations under the License.
 
 package postgres
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/jackc/pgproto3/v2"
+)
+
+var (
+	postgresProtocolVersion = []byte{'\x00', '\x03', '\x00', '\x00'}
+	cancelRequestPrefix     = bigEndianPackUint32(8, 80877102)
+	sslRequest              = bigEndianPackUint32(8, 80877103)
+	gssEncReq               = bigEndianPackUint32(8, 80877104)
+)
+
+// ParseChunk parses a TCP packet as a PostgreSQL packet. This assumes a
+// discrete TCP packet contains exactly one PostgreSQL packet, but this
+// assumption may be revisted at a later time.
+//
+// See:
+// - https://godoc.org/github.com/jackc/pgproto3
+// - https://www.postgresql.org/docs/13/protocol-message-formats.html
+func ParseChunk(chunk []byte) (pgproto3.FrontendMessage, error) {
+	fm, err := parseFrontendMessage(chunk)
+	if err != nil {
+		return nil, err
+	}
+
+	if fm != nil {
+		return fm, nil
+	}
+
+	if len(chunk) == 0 {
+		err := fmt.Errorf(
+			"%w; message must contain at least 8 bytes, has %d",
+			ErrParsingClientMessage, len(chunk),
+		)
+		return nil, err
+	}
+
+	if bytes.Equal(chunk[:8], cancelRequestPrefix) {
+		cr := &pgproto3.CancelRequest{}
+		err = cr.Decode(chunk[4:])
+		if err != nil {
+			return nil, err
+		}
+
+		return cr, nil
+	}
+
+	if bytes.Equal(chunk[:8], sslRequest) {
+		sr := &pgproto3.SSLRequest{}
+		return sr, nil
+	}
+
+	if bytes.Equal(chunk[:8], gssEncReq) {
+		ger := &pgproto3.GSSEncRequest{}
+		return ger, nil
+	}
+
+	if !bytes.Equal(chunk[4:8], postgresProtocolVersion) {
+		err = fmt.Errorf("%w; expected a startup message", ErrParsingClientMessage)
+		return nil, err
+	}
+
+	sm := &pgproto3.StartupMessage{}
+	err = sm.Decode(chunk[4:])
+	if err != nil {
+		return nil, err
+	}
+
+	return sm, nil
+}
+
+func parseFrontendMessage(chunk []byte) (pgproto3.FrontendMessage, error) {
+	if len(chunk) < 5 {
+		err := fmt.Errorf(
+			"%w; message must contain at least 5 bytes, has %d",
+			ErrParsingClientMessage, len(chunk),
+		)
+		return nil, err
+	}
+
+	messageType := chunk[0]
+	switch messageType {
+	case 'B':
+		b := &pgproto3.Bind{}
+		err := b.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return b, nil
+	case 'C':
+		c := &pgproto3.Close{}
+		err := c.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return c, nil
+	case 'd':
+		cd := &pgproto3.CopyData{}
+		err := cd.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return cd, nil
+	case 'c':
+		cd := &pgproto3.CopyDone{}
+		err := cd.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return cd, nil
+	case 'f':
+		cf := &pgproto3.CopyFail{}
+		err := cf.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return cf, nil
+	case 'D':
+		d := &pgproto3.Describe{}
+		err := d.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return d, nil
+	case 'E':
+		e := &pgproto3.Execute{}
+		err := e.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return e, nil
+	case 'H':
+		f := &pgproto3.Flush{}
+		err := f.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return f, nil
+	case 'F':
+		err := fmt.Errorf("%w; FunctionCall not supported by `pgx` project", ErrNotImplemented)
+		return nil, err
+	case 'P':
+		p := &pgproto3.Parse{}
+		err := p.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return p, nil
+	case 'Q':
+		q := &pgproto3.Query{}
+		err := q.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return q, nil
+	case 'S':
+		s := &pgproto3.Sync{}
+		err := s.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return s, nil
+	case 'X':
+		t := &pgproto3.Terminate{}
+		err := t.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return t, nil
+	case 'p':
+		// TODO: The `p` BYTE1 value can also represent
+		//       `GSSResponse`, `SASLInitialResponse` and `SASLResponse`
+		pm := &pgproto3.PasswordMessage{}
+		err := pm.Decode(chunk[5:])
+		if err == nil {
+			return pm, nil
+		}
+
+		sir := &pgproto3.SASLInitialResponse{}
+		err = sir.Decode(chunk[5:])
+		if err == nil {
+			return sir, nil
+		}
+
+		sr := &pgproto3.SASLResponse{}
+		err = sr.Decode(chunk[5:])
+		if err != nil {
+			return nil, err
+		}
+
+		return sr, nil
+	default:
+		return nil, nil
+	}
+}
+
+func bigEndianPackUint32(v ...uint32) []byte {
+	b := make([]byte, 4*len(v))
+	for i, element := range v {
+		binary.BigEndian.PutUint32(b[4*i:4*(i+1)], element)
+	}
+	return b
+}
